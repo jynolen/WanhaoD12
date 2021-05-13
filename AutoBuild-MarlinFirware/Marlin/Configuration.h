852d795804ed0177ed077348d699814cc71780d6
Jean-Yves NOLEN <jynolen@gmail.com>
2021-05-13 12:04:31 +0200

    Add invert E

--- AutoBuild-MarlinFirware/Marlin/Configuration.h
+++ AutoBuild-MarlinFirware/Marlin/Configuration.h
@@ -1191,8 +1191,8 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
-#define INVERT_E1_DIR false
+#define INVERT_E0_DIR WD12_INVERT_E0_DIR
+#define INVERT_E1_DIR WD12_INVERT_E1_DIR
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
 #define INVERT_E4_DIR false
